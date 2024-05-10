99bdfd715ba5c5189aedcd371645b1b337e78165
Heokunho <93777460+Heokunho@users.noreply.github.com>
2024-05-11 03:08:16 +0900

    Add : Vehicle

--- Project-UDT/Assets/HKH/Scripts/AutonomousVehicle.cs
+++ Project-UDT/Assets/HKH/Scripts/AutonomousVehicle.cs
@@ -6,6 +6,7 @@ public class AutonomousVehicle : MonoBehaviour
 {
     public Transform[] waypoints;
     public float speed = 5.0f;
+    private float rotationSpeed = 100.0f;  // 회전 속도
     private int waypointIndex = 0;
 
     private void Update()
@@ -20,6 +21,14 @@ public class AutonomousVehicle : MonoBehaviour
 
         Transform targetWaypoint = waypoints[waypointIndex];
         transform.position = Vector3.MoveTowards(transform.position, targetWaypoint.position, speed * Time.deltaTime);
+
+        // 오브젝트가 이동 방향을 바라보도록 회전
+        Vector3 direction = targetWaypoint.position - transform.position;
+        if (direction != Vector3.zero)  // 방향 벡터가 0이 아닐 때만 회전 처리
+        {
+            Quaternion toRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
 
         if (Vector3.Distance(transform.position, targetWaypoint.position) < 0.1f) // 거의 도착했을 때
         {
